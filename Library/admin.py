098ebc3afb91eb4ee1a63dedcffc91560da6d806
Saikat <saikat@Saikats-MacBook-Air.local>
2025-10-10 10:56:23 +0600

    Set up Django admin site configuration and register Department model

--- Library/admin.py
+++ Library/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Department
 
 # Register your models here.
+admin.site.site_header = "PyLibrary Admin"
+admin.site.site_title = "PyLibrary Super Admin Portal"
+admin.site.index_title = "Welcome to PyLibrary Admin Portal"
+
+# Department Admin
+admin.site.register(Department)
