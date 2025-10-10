dafa226ca68d67fd8213cf83b41dbbbc7af4c37e
0xh7ml <0xh7ml.py@gmail.com>
2025-10-10 06:42:51 +0600

    Register Student model in admin interface

--- Library/admin.py
+++ Library/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Department
+from .models import Department, Student
 
 # Register your models here.
 admin.site.site_header = "PyLibrary Admin"
@@ -8,3 +8,4 @@ admin.site.index_title = "Welcome to PyLibrary Admin Portal"
 
 # Department Admin
 admin.site.register(Department)
+admin.site.register(Student)
