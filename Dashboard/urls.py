598644aa8fc19b95a0f3793af3541e584057b42a
MD TAHER BIN OMAR HIJBULLAH <hijbullah119445@gmail.com>
2026-04-12 09:08:27 +0000

    refactor: update URLs for student list and remove app name from dashboard

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 
 from .views import home, student_list
 
-app_name = 'dashboard'
-
 urlpatterns = [
     path('', home, name='home'),
     path('students/', student_list, name='student_list'),
